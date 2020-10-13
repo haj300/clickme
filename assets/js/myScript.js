71f5d3a8f0f97bc53c2dff18bf7e3292acc7a366
haj300 <katja.lindeberg@gmail.com>
2020-10-13 09:54:39 +0200

    adding comments

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,25 +1,34 @@
-console.log("hello");
-
 
 $(document).ready(function(){
 
   // jQuery methods go here...
 
+/* om man klickar på sidan så försvinner texten i <p> */
   $("p").click(function(){
     $(this).hide("slow");
   });
 
 });
 
+/* här börjar vanlig javascript */
+
+/* för att se att javascript fungerar/skriver till consolen */
+console.log("hello");
+
+
+/* hämtar bodyn från htmlfilen och sätter bodyn i center */
 var body = document.querySelector("body");
 body.style.textAlign = "center";
 
+/* hämtar button från html för att kunna användas i den här filen */
 var button = document.getElementById("mybutton");
 console.log(button);
 
+/* hämtar h3an i html för att kunna uppdatera texten */
 var myresultdiv = document.querySelector(".myresult");
 var count = 0;
 
+/* "anonym funktion" (den har inget namn) som uppdaterar klicken och texten */
 button.onclick = function() {
   count = count + 1;
 
